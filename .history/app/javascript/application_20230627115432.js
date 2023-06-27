7211db1108e0be1a3a5716622afc09471034e5b5
ashavan33 <105936390+ashavan33@users.noreply.github.com>
2023-06-27 12:23:42 -0400

    revert?

---
+++ .history/app/javascript/application_20230627115432.js
@@ -0,0 +1,14 @@
+// Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
+import "@hotwired/turbo-rails"
+import "controllers"
+import "@hotwired/turbo-rails"  
+import "./controllers"  
+import "trix"
+import "@rails/actiontext"
+
+
+
+let popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))  
+let popoverList = popoverTriggerList.map(function (popoverTriggerEl) {  
+  return new bootstrap.Popover(popoverTriggerEl)  
+})  
--- .history/config/routes_20230522100002.rb
+++
@@ -1,9 +0,0 @@
-Rails.application.routes.draw do
-  get 'pages/home'
-  get 'pages/about'
-  get 'home/about'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
-end
